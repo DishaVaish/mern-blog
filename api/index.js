50ecb2ca6aeb4d2e3f69761686bd5e954d853440
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-04-14 11:29:34 +0530

    Update index.js till signup code

--- api/index.js
+++ api/index.js
@@ -1,17 +1,11 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-
-
-// const app=express();
-
-// app.listen(3000, () => {
-//     console.log('server is running on port 3000');
-
-// });
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.routes.js';
 
-mongoose.connect('mongodb://localhost:27017/Login-signup',{
+// mongoose.connect('mongodb://localhost:27017/Login-signup',{
+  // mongoose.connect('mongodb://127.0.0.1:27017/Login-signup', {
+  mongoose.connect('mongodb+srv://vedangmehta07:6S6sRQubtBGL293k@mern-blog.nfbdcuk.mongodb.net/mern-blog?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -23,11 +17,29 @@ mongoose.connect('mongodb://localhost:27017/Login-signup',{
   });
   
 const app = express();
+//As a default we are not allowed to send json to te backend
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get('/test', (req, res) => {
-    res.json({message: 'API is working!'})
-});
+//Here we are using app.use in place of app.get coz we r going to
+//use the get req here inside the index part
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+
+//middleware made
+app.use((err,req,res, next) =>{
+  const statusCode =err.statusCode || 500;
+  const message= err.message|| 'Internal Server Error ';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+
+  });
+} );
+
+
+    

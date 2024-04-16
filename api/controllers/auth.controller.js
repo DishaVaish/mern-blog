9a44a8bd53ed30d05c86666a533dfb0d7b009120
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-04-16 17:12:49 +0530

    Update auth.controller.js with signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next)=>{
   const { username, email, password }= req.body;
@@ -32,4 +33,38 @@ export const signup = async (req, res, next)=>{
 
 };
 
+export const signin = async (req, res, next)=>{
+  const {email, password} =req.body;
 
+  if(!email|| !password|| email===''||password===''){
+    return next (errorHandler(400, 'All fields are Required'));
+  }
+try{
+  //WE have to wait for the results: await
+    const validUser = await User.findOne({email});
+    if(!validUser){
+    return next(errorHandler(404, 'User not Found')); 
+    }
+  const validPassword= bcryptjs.compareSync(password , validUser.password);
+  if(!validPassword){
+    return next(errorHandler(400, 'Invalid Password'));  
+}
+
+  //For creating a unique token of signin: 
+  //It is going to be encrypted just like hashed password 
+  //_id is the unique id stored for each user in MongoDB
+  const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+  // const token = jwt.sign({id: validUser._id}, 'vsmehta');
+  // we can add expiresIn : 'Id' here so after how much time the seesion will be expired
+  const {password: pass, ...rest} = validUser._doc;
+
+  res.status(200).cookie('access_token', token, {
+    httpOnly: true}).json(validUser);
+
+  // res.cookie('access_token', token, { httpOnly: true, secure: true, sameSite: 'strict' });
+  //   res.status(200).json(rest);
+  }catch(error){
+    next(error);
+}
+
+}

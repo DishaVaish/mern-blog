3da6744222a34163a9268608d0855f88d553bfdf
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-04-17 17:56:21 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
 
 // mongoose.connect('mongodb://localhost:27017/Login-signup',{
   // mongoose.connect('mongodb://127.0.0.1:27017/Login-signup', {
-  mongoose.connect('mongodb+srv://vedangmehta07:6S6sRQubtBGL293k@mern-blog.nfbdcuk.mongodb.net/mern-blog?retryWrites=true&w=majority', {
-    //Warning: useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
-  //(Use `node --trace-warnings ...` to show where the warning was created)
-import postRoutes from './routes/post.route.js';
-
-mongoose.connect('mongodb://localhost:27017/Login-signup',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  // // mongoose.connect('mongodb+srv://vedangmehta07:6S6sRQubtBGL293k@mern-blog.nfbdcuk.mongodb.net/mern-blog?retryWrites=true&w=majority', {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // })
+  mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -27,10 +26,7 @@ app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-
 });
-
-app.use('/api/post',postRoutes);
 
 //Here we are using app.use in place of app.get coz we r going to
 //use the get req here inside the index part

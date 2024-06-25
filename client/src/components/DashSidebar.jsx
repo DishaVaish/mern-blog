6ed25c2524511ad117d730a6ad640690711c74ea
Disha <dishavaish11@gmail.com>
2024-06-25 13:59:34 +0530

    Create get users api route

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -24,18 +24,28 @@ export default function DashSidebar() {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item //both sidebar.item and link are <a> tag nested in each other will cause error
-                        active={tab === 'profile'} 
-                        icon={HiUser} 
-                        label={'User'} 
-                        labelColor='dark'
-                        as ='div'
-                        // onClick={() => handleNavigation('/dashboard?tab=profile')}
-                    >
-                        Profile
-                    </Sidebar.Item>
-                    </Link>
+                  <Link to='/dashboard?tab=profile'>
+                            <Sidebar.Item
+                            active={tab === 'profile'}
+                            icon={HiUser}
+                            label={currentUser.isAdmin ? 'Admin' : 'User'}
+                            labelColor='dark'
+                            as='div'
+                            >
+                            Profile
+                            </Sidebar.Item>
+                        </Link>
+                    {currentUser.isAdmin && (
+                        <Link to='/dashboard?tab=posts'>
+                        <Sidebar.Item
+                            active={tab === 'posts'}
+                            icon={HiDocumentText}
+                            as='div'
+                        >
+                            Posts
+                        </Sidebar.Item>
+                        </Link>
+                    )}
                     <Sidebar.Item 
                         icon={HiArrowSmRight} 
                         className='cursor-pointer'

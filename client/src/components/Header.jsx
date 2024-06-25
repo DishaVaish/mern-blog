081300b6a2b00abb300ef05ae98c0ca5cc2210ff
neha055 <nehasingh8151@gmail.com>
2024-06-25 14:21:44 +0530

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -178,11 +178,11 @@ export default function Header() {
 //           <Link to='/'>Home</Link>
 //         </Navbar.Link>
 //         <Navbar.Link active={path === '/about'} as={'div'}>
-//           <Link to='/about'>About</Link>
-//         </Navbar.Link>
-//         <Navbar.Link active={path === '/projects'} as={'div'}>
-//           <Link to='/projects'>Projects</Link>
-//         </Navbar.Link>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   )};
+  //         <Link to='/about'>About</Link>
+  //       </Navbar.Link>
+  //       <Navbar.Link active={path === '/projects'} as={'div'}>
+  //         <Link to='/projects'>Projects</Link>
+  //       </Navbar.Link>
+  //     </Navbar.Collapse>
+  //   </Navbar>
+  // )};

9e5ad673f4ea952a56b36fde99338674ebe9b189
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-07-01 22:21:52 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,43 @@
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className= "min-h-screen flex items-start justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold
+          text-center my-7 ">About MNNIT Blog</h1>
+          <div className=" text-md text-gray-500 flex flex-col gap-6 ">
+          <h4>
+          Welcome to MNNIT Blog!
+          </h4>
+          <p>We are a dedicated team of three passionate students from MNNIT, 
+            united by our shared vision to provide invaluable guidance and resources
+            to students navigating their academic and career journeys. MNNIT Blog 
+              is a hub for insightful blogs related to career guidance,
+              competitive exams like JEE, NEET, NDA, and GATE, 
+            as well as college-related topics such as internships, placements, and soft skills development.
+          </p>
+          <p>
+          At CareerVista, we understand the challenges and 
+          uncertainties that students face during their 
+          academic and professional paths. Our goal is to empower you 
+          with the knowledge, tips, and experiences shared by experts and peers, 
+          helping you make informed decisions and achieve your aspirations.
+          </p>
+          <p>
+          Thank you for visiting MNNIT Blog. Your trust and support inspire us
+           to continuously improve and expand our content to meet your needs. We
+            are committed to being your reliable companion in your educational 
+            and career pursuits. 
+          </p>
+          <p>
+          Warm regards,<br/>
+          Disha, Neha and Vedang<br/>
+          MNNIT Blog Team<br/>
+          </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

95ee98d027f040990729d10e4ef6d6de8aa6f8d3
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-07-01 22:20:46 +0530

    Update Projects.jsx

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,10 +1,16 @@
 // import React from 'react'
 
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
   return (
-    <div>
-      Projects
-
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center
+    items-center flex-col gap-6 p-3">
+     <h1 className = 'text-3xl font-semibold'>Projects..</h1>
+      <p className="text-md text-gray-500">Get blogs about Various Career Opportunities and get ready 
+         to ace your dream competitive exam
+      </p>
+      <CallToAction/>
     </div>
   )
 }

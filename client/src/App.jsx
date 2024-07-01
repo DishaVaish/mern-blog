ac8104ae26e853bc80c9cd272163d880e43fcb62
Vedang Mehta <139338917+VedangMehta45@users.noreply.github.com>
2024-07-01 22:13:39 +0530

    Update App.jsx (added search page)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost'
 // import Post from './../../api/models/post.model';
 import PostPage from './pages/PostPage';
+import Search from './pages/Search';
 import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
@@ -26,6 +27,7 @@ export default function App() {
          <Route path="/about" element={<About />} />
          <Route path="/sign-in" element={<SignIn />} />
          <Route path="/sign-up" element={<SignUp />} />
+         <Route path="/search" element={<Search />} />
          <Route element ={<PrivateRoute/>}>
          <Route path="/dashboard" element={<Dashboard />} />
          </Route>
